65920c7e9cdefac22873fa8199d90cc9376b995f
zkoenigxd <zkoenigxd@gmail.com>
2024-08-20 15:55:34 -0500

    Fixed video playback errors

--- Assets/Scripts/BasicVideoPlayer.cs
+++ Assets/Scripts/BasicVideoPlayer.cs
@@ -7,20 +7,32 @@ using UnityEngine.Video;
 public class BasicVideoPlayer : MonoBehaviour
 {
     VideoPlayer videoPlayer;
+    [SerializeField] RenderTexture videoTexture;
+    [SerializeField] Texture2D playerTexture;
     [SerializeField] SpriteRenderer spriteRenderer;
     [SerializeField] Sprite playSprite;
     [SerializeField] Sprite pauseSprite;
 
     private void OnEnable()
     {
+        if (playerTexture != null)
+            this.gameObject.GetComponent<Renderer>().material.mainTexture = playerTexture;
         videoPlayer = GetComponent<VideoPlayer>();
         spriteRenderer.sprite = playSprite;
+        videoPlayer.prepareCompleted += ConfigureTexture;
+    }
+
+    void ConfigureTexture(VideoPlayer vp)
+    {
+        vp.texture.wrapMode = TextureWrapMode.Mirror;
     }
 
     public void PlayOrPauseVideo()
     {
         if (videoPlayer != null)
         {
+            if (videoTexture != null)
+                this.gameObject.GetComponent<Renderer>().material.mainTexture = videoTexture;
             if (videoPlayer.isPlaying)
             {
                 videoPlayer.Pause();
